2416f9fc7e34eb3815e1273ff6985a816ad489f6
Espen Stene Brønstad <esb@mcash.no>
2013-08-21 16:35:44 +0200

    MVP-64 Restructured and updated setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='mcash.sphinx',
-    version='1.0',
+    version='0.1',
     author="mCASH Team",
     author_email="support@mca.sh",
     description="mCASH Sphinx modules",
-    packages=find_packages(),
+    packages=find_packages('mcash.sphinx'),
+    package_dir = {'': 'mcash.sphinx'},
     include_package_data=True,
     install_requires=[
         'sphinx'
     ],
-    namespace_packages=['mcash']
+    namespace_packages=['mcash'],
+    zip_safe=True,
 )
