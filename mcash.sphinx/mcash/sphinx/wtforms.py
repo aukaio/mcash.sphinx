d492ca65c884058be6704e0aed16e6c89d0703ac
Espen Stene Brønstad <esb@mcash.no>
2013-09-26 22:45:01 +0200

    MVP-240 WTForms using tables instead of definition list

--- mcash.sphinx/mcash/sphinx/wtforms.py
+++ mcash.sphinx/mcash/sphinx/wtforms.py
@@ -3,6 +3,7 @@ This Sphinx extension displays WTForms with fields
 """
 from docutils import nodes
 from sphinx.util.compat import Directive
+from sphinx.util.docstrings import prepare_docstring
 
 from mcash.utils import import_obj
 from mcash.core.forms.form import Form
@@ -18,7 +19,10 @@ validator_processors = {
     'wtforms.validators.MacAddress': lambda v: 'Valid MAC address',
     'wtforms.validators.NoneOf': lambda v: 'Value not in %s' % v.values_formatter(v.values),
     'wtforms.validators.NumberRange': lambda v: '%s <= value <= %s' % (v.min, v.max),
-    'wtforms.validators.Length': lambda v: '%s < len(value) < %s' % (v.min, v.max),
+    'wtforms.validators.Length': lambda v: '%slength%s' % (
+        '%s < ' % v.min if v.min > -1 else '',
+        ' < %s' % v.max if v.max > -1 else '',
+    ),
     'wtforms.validators.Optional': lambda v: 'Optional (stops validation if missing)',
     'wtforms.validators.Regexp': lambda v: 'Regexp: %s' % v.regex.pattern,
     'wtforms.validators.Required': lambda v: 'Data required (new or existing on update)',
@@ -32,6 +36,7 @@ class WTFormsDirective(Directive):
     # create corresponding docutils nodes, as the form tree and doc tree are
     # equivalent
     has_content = True
+    show_form_name = False
 
     def make_field(self, name, body):
         if not isinstance(body, nodes.Node):
@@ -57,7 +62,7 @@ class WTFormsDirective(Directive):
                 validators.append(nodes.list_item('', nodes.paragraph(text=vp(v))))
         return validators
 
-    def extract_field_properties(self, field, field_list=True):
+    def extract_field_properties(self, field, field_list=False):
         copy = ('default', 'description')
         properties = []
         for attr in copy:
@@ -70,31 +75,37 @@ class WTFormsDirective(Directive):
         properties.append(('validators', self.process_validators(field)))
         if field_list:
             return self.make_field_list(properties)
-        return properties
+        return dict(properties)
 
-    def make_definition_list(self, term, definition=None, classifier=None):
-        definition_list = nodes.definition_list_item()
+    def make_definition_list(self, parent, term, definition=None, classifier=None):
+        left_cell, right_cell = self.prepare_cells(parent)
         if not isinstance(term, nodes.Node):
             term = nodes.strong(text=term)
-        definition_list.append(term)
+        left_cell.append(term)
         if classifier is not None:
-            definition_list.append(nodes.classifier(text=classifier))
+            left_cell.append(nodes.classifier(text=classifier))
         if definition is not None:
-            if isinstance(definition, (list, tuple)):
-                definition_list.append(nodes.definition('', *definition))
-            else:
-                definition_list.append(nodes.definition('', definition))
-
-        return definition_list
+            required = definition.get('required', False)
+            left_cell.append(nodes.classifier('', 'required' if required else 'optional'))
+            defs = []
+            if not required and 'default' in definition:
+                left_cell.append(nodes.classifier('', definition['default']))
+            if 'validators' in definition:
+                defs.extend(definition['validators'])
+            left_cell.extend(nodes.paragraph('', d) if not isinstance(d, nodes.Node) else d for d in defs)
+            for name in ('label', 'description'):
+                value = definition.get(name)
+                if value:
+                    right_cell.append(nodes.paragraph('', '\n'.join(prepare_docstring(value))))
+        return left_cell, right_cell
 
     def process_field_generic(self, field, parent):
         properties = self.extract_field_properties(field)
-        parent.append(
-            self.make_definition_list(
-                term=field.name,
-                definition=properties,
-                classifier=field.type,
-            )
+        self.make_definition_list(
+            parent,
+            term=field.name,
+            definition=properties,
+            classifier=field.type,
         )
 
     def process_field_FieldList(self, field, parent):
@@ -104,14 +115,13 @@ class WTFormsDirective(Directive):
         self.process_field_delegate(subfield, parent)
 
     def process_field_FormField(self, field, parent):
-        definition_list = self.make_definition_list(
+        left_cell, right_cell = self.make_definition_list(
+            parent,
             term=field.name,
             classifier=field.type,
         )
 
-        self.process_form(field.form_class, definition_list)
-
-        parent.append(definition_list)
+        self.process_form(field.form_class, right_cell)
 
     def process_field_delegate(self, field, parent):
         if getattr(field.__class__, 'model_helper', False):  # Avoid recursion on FormField
@@ -120,13 +130,37 @@ class WTFormsDirective(Directive):
         process_field_method = getattr(self, 'process_field_%s' % field.type, getattr(self, 'process_field_generic'))
         process_field_method(field, parent)
 
+    def prepare_table(self, parent):
+        tbody = nodes.tbody(
+            '',
+        )
+        table = nodes.table(
+            '',
+            nodes.tgroup(
+                '',
+                nodes.colspec(colwidth=1, classes=['foo-raw']),
+                nodes.colspec(colwidth=1),
+                tbody,
+            ),
+            classes=['foo']
+        )
+        parent.append(table)
+        return tbody
+
+    def prepare_cells(self, parent):
+        left_cell, right_cell = nodes.entry(), nodes.entry()
+        row = nodes.row('', left_cell, right_cell)
+        parent.append(row)
+        return left_cell, right_cell
+
     def process_form(self, formclass, parent):
         assert issubclass(formclass, Form)
-        fields = nodes.definition_list()
-        parent.append(nodes.definition('', fields))
+        fields = self.prepare_table(parent)
         forminstance = formclass()
 
         for field in forminstance:
+            if field.name == 'link':
+                continue
             self.process_field_delegate(field, fields)
 
     def run(self):
@@ -136,12 +170,13 @@ class WTFormsDirective(Directive):
 
         root = nodes.definition_list()
         module, classname = obj_path.rsplit('.', 1)
-        root.append(nodes.term(
-            None,
-            '',
-            nodes.emphasis(text='Form '),
-            nodes.literal(text='%s.' % module),
-            nodes.strong(text=classname)))
+        if self.show_form_name:
+            root.append(nodes.term(
+                None,
+                '',
+                nodes.emphasis(text='Form '),
+                nodes.literal(text='%s.' % module),
+                nodes.strong(text=classname)))
 
         self.process_form(formclass, root)
 
