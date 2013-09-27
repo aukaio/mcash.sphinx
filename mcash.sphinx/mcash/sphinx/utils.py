b9cc046545309a7311f5e6557dd30c4e958759ff
Espen Stene Brønstad <esb@mcash.no>
2013-09-27 15:51:05 +0200

    MVP-240 Added @not_implemented decorator

--- mcash.sphinx/mcash/sphinx/utils.py
+++ mcash.sphinx/mcash/sphinx/utils.py
@@ -1,7 +1,8 @@
+import functools
 from importlib import import_module
 
 
-__all__ = ['import_obj', 'get_import_path']
+__all__ = ['import_obj', 'get_import_path', 'not_implemented']
 
 
 def import_obj(path):
@@ -9,7 +10,7 @@ def import_obj(path):
     curr_path = parts.pop(0)
     obj = import_module(curr_path)
     for part in parts:
-        curr_path += '.' +  part
+        curr_path += '.' + part
         try:
             obj = getattr(obj, part)
         except AttributeError:
@@ -21,3 +22,11 @@ def get_import_path(obj):
     return obj.__module__ + '.' + obj.__name__
 
 
+def not_implemented(reason=''):
+    def decorator(f):
+        @functools.wraps(f)
+        def new_f(*args, **kwargs):
+            raise NotImplementedError
+        new_f._not_implemented = reason
+        return new_f
+    return decorator
