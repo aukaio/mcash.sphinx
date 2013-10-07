1d408e9e93099dcb5f689495bebbe6dc8c5e9aac
Espen Stene Brønstad <esb@mcash.no>
2013-10-07 22:02:18 +0200

    MVP-240 HTML writer with CSS class replacement functionality

--- mcash.sphinx/mcash/sphinx/writers.py
+++ mcash.sphinx/mcash/sphinx/writers.py
@@ -1,6 +1,12 @@
 from sphinx.writers.html import HTMLTranslator
 
 
+replace_classes = {
+    'section': 'mcash-docs-section',
+    'sub-section': 'mcash-sub-section',
+}
+
+
 class StrippedHTMLTranslator(HTMLTranslator):
     def visit_table(self, node):
         self.context.append(self.compact_p)
@@ -8,3 +14,14 @@ class StrippedHTMLTranslator(HTMLTranslator):
         classes = 'table table-striped'
         self.body.append(
             self.starttag(node, 'table', CLASS=classes, border="0", width="100%", cellspacing="0", cellpadding="0"))
+
+    def starttag(self, node, tagname, suffix='\n', empty=False, **attributes):
+        for (name, value) in attributes.items():
+            attributes[name.lower()] = value
+
+        classes = attributes.get('class')
+        if classes:
+            classes = filter(None, map(lambda c: replace_classes.get(c, c), classes.split()))
+            attributes['class'] = ' '.join(classes)
+        return HTMLTranslator.starttag(self, node, tagname, suffix=suffix, empty=empty, **attributes)
+
